0dc5fcbfd50f99d1ce6efb032fb796938804c327
Howard Luo <info@luohuizhe.com>
2023-07-18 14:47:01 +1000

    update app

--- python-app/app.py
+++ python-app/app.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "OK"
+    try:
+        cnx = mysql.connector.connect(user='admin', password='password',
+                                      host='<rds-endpoint>'
+                                    )
+        if cnx.is_connected():
+            return 'connected'
+        else:
+            return 'not connected'
+    except mysql.connector.Error as err:
+        return f"Something went wrong: {err}"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
