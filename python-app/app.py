de00a2e622e4b4602d5e781d78ba6792dbb9efde
Wheel Inventor <info@luohuizhe.com>
2023-07-20 10:41:35 +1000

    update app.py to be able to pass in the parameters

--- python-app/app.py
+++ python-app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 import mysql.connector
+import argparse
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     try:
-        cnx = mysql.connector.connect(user='admin', password='password',
-                                      host='<rds-endpoint>'
-                                    )
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--user", required=True, help="Database user")
+        parser.add_argument("--password", required=True, help="Database password")
+        parser.add_argument("--host", required=True, help="Database host")
+        args = parser.parse_args()
+
+        cnx = mysql.connector.connect(user=args.user, password=args.password, host=args.host)
+
         if cnx.is_connected():
             return 'connected'
         else:
